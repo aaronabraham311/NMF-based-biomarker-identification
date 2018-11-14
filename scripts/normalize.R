85058441340b0099dd12f8ee098f5c3efa86f0b6
Aaron Abraham <aaronabraham311@gmail.com>
2018-11-13 19:40:34 -0700

    Adding normalization and projection files

---
+++ scripts/normalize.R
@@ -0,0 +1,9 @@
+# NMF-based machine learning to identify biomarkers for Alzheimer's disease diagnosis
+# Author: Aaron Abraham
+# Date: November 13, 2018
+
+# TO DO
+# Fold selection
+# Threshold, ceiling
+# Column rank normalization
+# Variation filter
---
+++ scripts/project.R
@@ -0,0 +1,10 @@
+# NMF-based machine learning to identify biomarkers for Alzheimer's disease diagnosis
+# Author: Aaron Abraham
+# Date: November 13, 2018
+
+# TO DO:
+# Project NMF
+# Save H and W matrices
+# Construct projection 
+# Return transformed data and all matrices
+
