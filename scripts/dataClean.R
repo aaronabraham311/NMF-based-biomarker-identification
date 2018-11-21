f2f055e5a95e1d431da1db1c99029efb3bd2c04e
Aaron Abraham <aaronabraham311@gmail.com>
2018-11-20 21:48:06 -0700

    Modifying NA, row and column removal. Added labelling function

--- scripts/dataClean.R
+++ scripts/dataClean.R
@@ -12,6 +12,8 @@ cleanData <- function (
   raw <- read.csv(data.address);
   
   noNullandNA <- eliminateNullandNA(raw)
+  labelledData <- labelData(noNullandNA, key.address)
+  
   cleanData <- noNullandNA[!rowNumber, !columnData]
   return (c(cleanData, nrow(cleanData), ncol(cleanData)))
 }
@@ -21,8 +23,23 @@ eliminateNullandNA <- function (
   data) {
    noNull <- na.omit(data)
    
-   nonZeroRows <- apply(noNull, 1, function(row) all(row != 0))
-   noZero <- noNull[nonZeroRows, ]
+   nonZeroColumns <- apply(noNull, 2, function(col) all(col != 0)) # Creates list of columns that have non-zero columns
+   noZero <- noNull[, nonZeroColumns] # Subsetting data
+   print(c("Number of removed columns due to non-zero conditions: ", ncol(data) - length(nonZeroColumns))) # Outputting number of removed columns
    
    return(noZero)
+}
+
+labelData <- function(
+  data,
+  key.address) {
+  
+  # Downloading key data
+  keyData <- read.csv(key.address)
+  keyData <- keyData[,c("RID", "DXCURREN")] #RID serves as joiner, DXCURREN is diagnosis indicator. Possible extension: use columns that indicate diagnosis change
+  colnames(keyData) <- c("rid", "diagnosis") # Renaming columns
+  
+  labelledData <- merge(data, keyData, by = "rid")
+  
+  return (labelledData)
 }
