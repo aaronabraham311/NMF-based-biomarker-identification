3f6b6d5593027e3b648723585aeb34ebfbc56f9c
Aaron Abraham <aaronabraham311@gmail.com>
2019-01-02 21:09:48 -0700

    Creating accuracy and MCC functions

--- scripts/ml.R
+++ scripts/ml.R
@@ -18,4 +18,28 @@ machineLearning <- function (
   train[,predictor] <- as.factor(train[,predictor])
   test[,predictor] <- as.factor(test[,predictor])
   
+  # Shuffling training set
+  train <- train[sample(nrow(train)), ]
+}
+
+# Function for accuracy
+accuracyMetric <- function (conMatrix) {
+  tp <- conMatrix[1,1]
+  fp <- conMatrix[1,2]
+  fn <- conMatrix[2,1]
+  tn <- conMatrix[2,2]
+  
+  accuracy <- (tp + tn)/(tp + fp + fn + tn)
+  return(accuracy)
+}
+
+# Function for Matthew's correlation coefficient
+mcc <- function(conMatrix) {
+  tp <- conMatrix[1,1]
+  fp <- conMatrix[1,2]
+  fn <- conMatrix[2,1]
+  tn <- conMatrix[2,2]
+  
+  metric <- ((tp * tn) - (fp * fn))/(sqrt((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn)))
+  return(metric)
 }
