237c8fd86c4d1c7cf186ba246769c5f1426a78f1
Aaron Abraham <aaronabraham311@gmail.com>
2019-01-02 21:19:15 -0700

    Created general train and predict function

--- scripts/ml.R
+++ scripts/ml.R
@@ -9,7 +9,7 @@
 # Libraries
 library(caret)
 
-machineLearning <- function (
+baseML <- function (
   train, 
   test,
   predictor) {
@@ -20,6 +20,31 @@ machineLearning <- function (
   
   # Shuffling training set
   train <- train[sample(nrow(train)), ]
+  
+  # Random forest
+}
+
+trainPredict <- function (
+  train,
+  test,
+  method,
+  controlParameters) {
+  # Training model
+  model <- train(diagnosis ~.,
+                 data = train,
+                 method = method,
+                 trControl = controlParameters)
+  
+  # Predictions and accuracy metrics
+  testPredictions <- predict(model, test) # Could use probabilities
+  confMatrix <- table(predictions = testPredictions, actual = test$diagnosis)
+  accuracyMetric <- accuracy(confMatrix)
+  mccMetric <- mcc(confMatrix)
+  
+  returnValues <- list("model" = model, "testPredictions" = testPredictions, 
+                       "confusionMatrix" = confMatrix, "accuracy" = accuracyMetric,
+                       "mcc" = mccMetric)
+  return(returnValues)
 }
 
 # Function for accuracy
